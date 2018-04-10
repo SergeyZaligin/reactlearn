17a1f432f17cd5f118443ba3300291f75073fb43
cobweb <codename.cobweb@gmail.com>
2018-04-10 18:17:39 +0300

    fix

---
+++ react-webpack/src/main.js
@@ -0,0 +1,5 @@
+const text = 'Hello world!!!';
+
+//console.log(`This is ${ text }`);
+
+alert(`This is ${ text }`);
--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -1,19 +1,20 @@
 const NODE_ENV = process.env.NODE_ENV || 'development'; 
 
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public', 'dist')
   },
   devtool: NODE_ENV === 'development' && 'source-map',
   watch: true,
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js|jsx$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
